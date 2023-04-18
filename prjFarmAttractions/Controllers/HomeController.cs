98766df87ebd26ceaa70c518322f51a98fe0e109
3A732087 <quannn0180716v@gmail.com>
2023-04-18 15:38:51 +0800

    Controller撰寫、畫面調整

--- prjFarmAttractions/Controllers/HomeController.cs
+++ prjFarmAttractions/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using prjFarmAttractions.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +13,25 @@ namespace prjFarmAttractions.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            //下載全國休閒農業區旅遊資訊JSON開放資料
+            string url = "https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvAttractions.aspx";
+            HttpClient httpClient = new HttpClient();
+            httpClient.MaxResponseContentBufferSize = Int32.MaxValue;
+            var response = await httpClient.GetStringAsync(url);
+            string path = $"{Server.MapPath("JSON").Replace("HOME\\", "")}\\ODwsvAttractions.json"; ;
+            FileInfo fileInfo = new FileInfo(path);
+            StreamWriter streamWriter = new StreamWriter(path, false, System.Text.Encoding.UTF8);
+            streamWriter.WriteLine(response);
+            streamWriter.Close();
+            //將資訊JSON資料反序列化成FarmAttractions陣列物件
+            StreamReader streamReader = new StreamReader(path);
+            FarmAttractions[] farmAttractions = JsonConvert.DeserializeObject<FarmAttractions[]>(streamReader.ReadToEnd());
+            //將陣列物件先依City遞增排序，接著再依Town鄉鎮遞增排序
+            //將排序後的陣列物件傳送至View
+            return View(farmAttractions.OrderBy(m => m.City).ThenBy(m => m.Town).ToList());
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
