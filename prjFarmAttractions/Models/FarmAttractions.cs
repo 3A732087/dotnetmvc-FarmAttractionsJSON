73cc314bd5ef427b2a7faf4e2bb1cb94bf0bc0dd
3A732087 <quannn0180716v@gmail.com>
2023-04-18 15:38:20 +0800

    MODEL新增

---
+++ prjFarmAttractions/Models/FarmAttractions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjFarmAttractions.Models
+{
+    public class FarmAttractions
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string Introduction { get; set; }
+        public string TrafficGuidlines { get; set; }
+        public string Address { get; set; }
+        public string OpenHours { get; set; }
+        public string City { get; set; }
+        public string Town { get; set; }
+        public string Photo { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
+    }
+}
